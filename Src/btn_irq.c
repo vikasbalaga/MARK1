3bed89d7657900a75d03edaeb7f6001cbf480993
vikasbalaga <vikas@vbalagalt02.brainchipinc.local>
2025-07-01 08:14:54 +0000

    feat(stm32/app): add sample applications
    - applications use gpio driver to toggle on board leds using polling
      and interrupt methods

---
+++ Src/btn_irq.c
@@ -0,0 +1,74 @@
+/*
+ * btn_irq.c
+ *
+ *  Created on: Jul 1, 2025
+ *      Author: vbalaga
+ */
+#include <stdint.h>
+#include <stdio.h>
+#include "stm32f407xx_gpio.h"
+
+#if !defined(__SOFT_FP__) && defined(__ARM_FP)
+  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#endif
+
+void delay() {
+	for (int i = 0; i < 200000; i ++)
+		;
+}
+
+int main(void)
+{
+	//printf("Hello World\n");
+    /* Loop forever */
+	//for(;;);
+	GPIO_Handle_t led_gpio, btn_gpio;
+	led_gpio.pGPIOx = GPIOD;
+	led_gpio.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;
+	led_gpio.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUTPUT;
+	led_gpio.GPIO_PinConfig.GPIO_PinSpeed = GPIO_OUTPUT_SPEED_VERY_HIGH;
+	led_gpio.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	led_gpio.GPIO_PinConfig.GPIO_PinOPType = GPIO_OUTPUT_MODE_PUPL;
+	GPIO_Clock_Ctrl(GPIOD, 1);
+	GPIO_Init(&led_gpio);
+	
+	led_gpio.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	GPIO_Init(&led_gpio);
+
+	led_gpio.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;
+	GPIO_Init(&led_gpio);
+
+	led_gpio.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_15;
+	GPIO_Init(&led_gpio);
+
+	btn_gpio.pGPIOx = GPIOA;
+	btn_gpio.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_0;
+	btn_gpio.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IT_RFT;
+	btn_gpio.GPIO_PinConfig.GPIO_PinSpeed = GPIO_OUTPUT_SPEED_VERY_HIGH;
+	btn_gpio.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	btn_gpio.GPIO_PinConfig.GPIO_PinOPType = GPIO_OUTPUT_MODE_PUPL;
+	GPIO_Clock_Ctrl(GPIOA, 1);
+	GPIO_IRQConfig(EXTI0_IRQ_NUM, NVIC_IRQ_PRIORITY_12, 1);	
+	GPIO_Init(&btn_gpio);
+    /* Loop forever */
+	for(;;);
+}
+
+void toggle_led(){
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
+	delay();
+}
+
+void EXTI0_IRQHandler() {
+	GPIO_IRQHandler(GPIO_PIN_NO_0);
+	toggle_led();
+}
+
+
+
---
+++ Src/led_gpio.c
@@ -0,0 +1,56 @@
+/*
+ * led_gpio.c
+ *
+ *  Created on: Jun 14, 2025
+ *      Author: vbalaga
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include "stm32f407xx_gpio.h"
+
+#if !defined(__SOFT_FP__) && defined(__ARM_FP)
+  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#endif
+
+void delay() {
+	for (int i = 0; i < 1000000; i ++)
+		;
+}
+
+int main(void)
+{
+	//printf("Hello World\n");
+    /* Loop forever */
+	//for(;;);
+	GPIO_Handle_t led_gpio;
+	led_gpio.pGPIOx = GPIOD;
+	led_gpio.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;
+	led_gpio.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUTPUT;
+	led_gpio.GPIO_PinConfig.GPIO_PinSpeed = GPIO_OUTPUT_SPEED_VERY_HIGH;
+	led_gpio.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	led_gpio.GPIO_PinConfig.GPIO_PinOPType = GPIO_OUTPUT_MODE_PUPL;
+	GPIO_Clock_Ctrl(GPIOD, 1);
+	GPIO_Init(&led_gpio);
+	
+	led_gpio.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	GPIO_Init(&led_gpio);
+
+	led_gpio.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;
+	GPIO_Init(&led_gpio);
+
+	led_gpio.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_15;
+	GPIO_Init(&led_gpio);
+
+	while(1) {
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13);
+	delay();
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
+	delay();
+	}
+}
+
